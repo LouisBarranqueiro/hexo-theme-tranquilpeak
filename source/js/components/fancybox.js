c464235e139e9edc1c89c93d496fd3aec1fbfe82
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-03-02 16:12:41 +0100

    Add resources of fancybox-thumb helper

--- source/js/components/fancybox.js
+++ source/js/components/fancybox.js
@@ -1,7 +1,8 @@
 +function($) {
     $(document).ready(function() {
+        // Fancybox
         $(".fancybox").fancybox({
-            maxWidth	: 1000,
+            maxWidth	: 900,
             maxHeight	: 800,
             fitToView	: false,
             width		: '70%',
@@ -10,6 +11,27 @@
             closeClick	: false,
             openEffect	: 'none',
             closeEffect	: 'none'
+        });
+
+        // Fancybox-thumb helper
+        $(".fancybox-thumb").fancybox({
+            prevEffect	: 'none',
+            nextEffect	: 'none',
+            helpers	: {
+                title	: {
+                    type: 'outside'
+                },
+                thumbs	: {
+                    width	: 70,
+                    height	: 70
+                }
+            }
+        });
+
+        // Fancybox button to launch a fancybox with an external button
+        $('.fancybox-button').click(function(e) {
+            e.preventDefault();
+            $('.fancybox-thumb:eq(0)').click();
         });
     });
 }(jQuery);
