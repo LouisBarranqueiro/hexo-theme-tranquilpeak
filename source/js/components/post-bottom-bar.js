dc62af556aaccd639d64801692f33d2bf4ddf514
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-03-08 20:49:39 +0100

    Add js function to animate the post bottom bar

---
+++ source/js/components/post-bottom-bar.js
@@ -0,0 +1,21 @@
++function ($) {
+     'use strict';
+
+     $(document).ready(function() {
+         var $postBootmBar     = $('.post-bottom-bar');
+         var $postFooterElem   = $('.post-footer');
+         var $window           = $(window);
+         var postFooterElemPos = $postFooterElem.offset().top + $postFooterElem.height();
+
+         // Detect the scroll
+         $window.scroll(function () {
+             // Check if the post-footer div is visible by the user
+             if (($window.scrollTop() + $window.height()) > postFooterElemPos) {
+                $postBootmBar.slideUp();
+             }
+             else {
+                $postBootmBar.slideDown();
+             }
+         })
+     })
+ }(jQuery);
