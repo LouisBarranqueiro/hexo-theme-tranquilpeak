3c3a33b5a0c69817e08c41870fccf298d338ea03
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-03-15 17:29:55 +0100

    Fix a bug when the post footer is already visible

--- source/_js/post-bottom-bar.js
+++ source/_js/post-bottom-bar.js
@@ -1,23 +1,65 @@
 +function ($) {
-     'use strict';
+     'use strict'
 
-     $(document).ready(function() {
-         if ($('.post-bottom-bar').length) {
-             var $postBootmBar = $('.post-bottom-bar');
-             var $postFooterElem = $('.post-footer');
-             var $window = $(window);
-             var postFooterElemPos = $postFooterElem.offset().top + $postFooterElem.height();
+    /**
+     * Global variables
+     */
+    var $postFooterElem   = $('.post-footer');
+    var postFooterElemPos = $postFooterElem.offset().top + $postFooterElem.height(); // Position of the post footer element from the top page
 
-             // Detect the scroll
-             $window.scroll(function () {
-                 // Check if the post-footer div is visible by the user
-                 if (($window.scrollTop() + $window.height()) > postFooterElemPos) {
-                     $postBootmBar.slideUp();
-                 }
-                 else {
-                     $postBootmBar.slideDown();
-                 }
-             })
-         }
-     })
+    /**
+     *  PostBottomBar
+     *
+     * @param postBottomBar
+     * @constructor
+     */
+    var PostBottomBar = function(postBottomBar) {
+        this.$elem     = $('.post-bottom-bar');
+    }
+
+    /**
+     * Init the post bottom bar
+     */
+    PostBottomBar.prototype.init = function() {
+        var self = this;
+
+        self.checkPosition();
+
+        $(window).scroll(function () {
+            self.checkPosition();
+        })
+    };
+
+    /**
+     * Check if the post footer is visible by the user
+     */
+    PostBottomBar.prototype.checkPosition = function() {
+        if (($(window).scrollTop() + $(window).height()) > postFooterElemPos) {
+            this.slideUp();
+        }
+        else {
+            this.slideDown();
+        }
+    };
+
+    /**
+     * Slide up the post bottom bar
+     */
+    PostBottomBar.prototype.slideUp = function() {
+        this.$elem.slideUp();
+    };
+
+    /**
+     * Slide down the post bottom bar
+     */
+    PostBottomBar.prototype.slideDown = function() {
+        this.$elem.slideDown();
+    };
+
+    $(document).ready(function() {
+        if ($('.post-bottom-bar').length) {
+            var postBottomBar = new PostBottomBar('.post-bottom-bar');
+            postBottomBar.init();
+        }
+    })
  }(jQuery);
