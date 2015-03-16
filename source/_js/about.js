2d7c6ac3d174862847f3e3d8e82beb6ab8de7eb8
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-03-16 18:36:54 +0100

    Improve scalibility function

--- source/_js/about.js
+++ source/_js/about.js
@@ -1,36 +1,77 @@
 +function ($) {
     'use strict';
 
-    function slideBlog() {
-        var $blog = $('#blog');
-        var blogWidth = $('#blog').width();
-
-        $blog.animate({
-            left: '-=' + blogWidth,
-        }, 500, function() {
-            showAbout();
-            verticalAlignAbout();
-        });
-
-    };
-
-    function verticalAlignAbout() {
-        var $aboutAuthor = $('#about-author');
-        var aboutAuthorHeight = $('#about-author').height();
-        console.log('authorheighr:' + aboutAuthorHeight);
-        $aboutAuthor.css('margin-top', (($(window).height() / 2) - (aboutAuthorHeight / 2)) + 'px');
-
-    };
-
     function showAbout() {
+        $.when(fadeOutBlog()).done(function() {
+            dropAboutAuthor();
+        });
+        fadeInAbout();
+    };
+
+    function hideAbout() {
+        $.when(upAboutAuthor()).done(function() {
+            fadeInBlog();
+            fadeOutAbout();
+        });
+    };
+
+    function fadeOutBlog() {
+        var $blog = $('#blog');
+
+        $blog.fadeOut(500);
+    };
+
+    function fadeInBlog() {
+        var $blog = $('#blog');
+
+        $blog.fadeIn(500);
+    };
+
+    function fadeInAbout() {
         var $about = $('#about');
-        $about.fadeIn();
+
+        $about.fadeIn(500);
+    };
+
+    function fadeOutAbout() {
+        var $about = $('#about');
+
+        $about.fadeOut(500);
+    };
+
+    function dropAboutAuthor() {
+        var $aboutAuthor = $('#about-author');
+        var aboutAuthorHeight = $('#about-author').innerHeight();
+
+        $aboutAuthor
+            .css('top','0px')
+            .css('top','-' + aboutAuthorHeight + 'px')
+            .show()
+            .animate({
+                top: '+=' + (($(window).height() / 2) - aboutAuthorHeight - (aboutAuthorHeight/2)) + 'px'
+            });
+
+    };
+
+    function upAboutAuthor() {
+        var $aboutAuthor = $('#about-author');
+        var aboutAuthorHeight = $('#about-author').innerHeight();
+
+        $aboutAuthor
+            .animate({
+                top: '-=' + ((($(window).height() / 2) - (aboutAuthorHeight)) - (aboutAuthorHeight / 2)) + 'px'
+            })
+            .hide();
     };
 
     $(document).ready(function() {
         $('a[href=\'/about\']').click(function(e) {
             e.preventDefault();
-            slideBlog();
+            showAbout();
+        })
+        $('#about-btn-close').click(function(e) {
+            e.preventDefault();
+            hideAbout();
         })
     })
 
