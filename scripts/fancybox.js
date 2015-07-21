43a06a5c0b9b9f29e295c54f8bf51b01f220802f
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-07-21 22:07:22 +0200

    Fancybox can display inline and thumbnail images

--- scripts/fancybox.js
+++ scripts/fancybox.js
@@ -1,24 +1,32 @@
-var rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/;
+var pathRegex       = /(((?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[.\!\/\\w]*))?)/;
+var classRegex = /^[a-zA-Z0-9-_]+$/;
 
 /**
  * Fancybox tag
  *
  * Syntax:
- *   {% fancybox /path/to/image [/path/to/thumbnail] [title] %}
+ *   {% fancybox [class name] /path/to/image [/path/to/thumbnail] [title] %}
  */
-
 hexo.extend.tag.register('fancybox', function(args) {
-    var original  = args.shift(),
-        thumbnail = '';
+    console.log(args);
 
-    if (args.length && rUrl.test(args[0])) {
+    var original,
+        thumbnail = '',
+        cssClass  = '';
+
+    if (args.length && classRegex.test(args[0])) {
+        cssClass = args.shift();
+    }
+    original = args.shift();
+
+    if (args.length && pathRegex.test(args[0])) {
         thumbnail = args.shift();
     }
 
     var title = args.join(' ');
 
     return '<a class="fancybox" href="' + original + '" title="' + title + '">' +
-        '<img src="' + (thumbnail || original) + '" alt="' + title + '">' +
+        '<img class="' + cssClass + '" src="' + (thumbnail || original) + '" alt="' + title + '">' +
         '</a>' +
-        (title ? '<span class="caption">' + title + '</span>' : '');
+        (title && cssClass !== 'inline' ? '<span class="caption">' + title + '</span>' : '');
 });
