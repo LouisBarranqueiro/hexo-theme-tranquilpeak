4a06bf001cb74da1a8f0a4d2d2ba5b54adddb15c
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-10-08 20:29:10 +0200

    remove useless filter

--- scripts/filters/front-matter-excerpt.js
+++
@@ -1,8 +0,0 @@
-var ymf = require('yaml-front-matter');
-
-hexo.extend.filter.register('after_post_render', function(data) {
-    if (ymf.loadFront(data.raw).excerpt) {
-        data.excerpt = ymf.loadFront(data.raw).excerpt + "<br>";
-    }
-    return data;
-});
