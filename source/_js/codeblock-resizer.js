daf9e1cf9057a4dccb2e68911e802a6291ad4354
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2015-12-20 17:37:56 +0100

    fix bug : codeblock-resizer wasn't working when there was no figcaption

--- source/_js/codeblock-resizer.js
+++ source/_js/codeblock-resizer.js
@@ -26,13 +26,12 @@
          */
         resize: function() {
             var self = this;
-
             self.$codeBlocks.each(function() {
                 var $gutter = $(this).find('.gutter');
                 var $code = $(this).find('.code');
                 // get padding of code div
                 var codePaddings = $code.width() - $code.innerWidth();
-                // figcaption div width with padding - gutter div with padding + code div padding
+                // codeblock div width with padding - gutter div with padding + code div padding
                 var width = $(this).outerWidth() - $gutter.outerWidth() + codePaddings;
                 // apply new width
                 $code.css('width', width);
