99ace23be499805b844455885e9e9e036805ea5c
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-02-11 16:19:56 +0100

    fix bug #204 - line numbers and line of codes wasn't aligned when code block had a scroll bar

--- source/_js/codeblock-resizer.js
+++ source/_js/codeblock-resizer.js
@@ -36,11 +36,21 @@
                 // apply new width
                 $code.css('width', width);
                 $code.children('pre').css('width', width);
+                // Add extra padding-bottom to gutter
+                if ($code.find('pre').hasHorizontalScrollBar()) {
+                    // 21px : perfect height to simulate a scroll bar added to the gutter
+                    // used to align line number with line of code
+                    $gutter.css('padding-bottom', '21px');
+                }
             });
         }
     };
 
     $(document).ready(function() {
+        // register jQuery function to check if an element has an horizontal scroll bar
+        $.fn.hasHorizontalScrollBar = function() {
+            return this.get(0).scrollWidth > this.innerWidth();
+        };
         var resizer = new CodeBlockResizer('figure.highlight');
         resizer.run();
     });
