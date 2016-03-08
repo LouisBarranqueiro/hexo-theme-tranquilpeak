1f4736478671786ac3586269cb18a9a25ae2f194
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-03-08 17:58:11 +0100

    improve migrator script

--- scripts/migrators/1.7.0.js
+++ scripts/migrators/1.7.0.js
@@ -17,13 +17,11 @@
      * @param {function} callback
      */
     function checkFile(filename, callback) {
-      var postsDir = path.resolve(process.cwd(), path.resolve(hexo.config.source_dir, '_tests'));
       var rExcerpt = /(?:<!-- ?more ?-->|<!-- ?excerpt ?-->)/;
       var rFilename = /.md$/;
-      var filepath = path.resolve(postsDir, filename);
       // check that file is a markdown file
       if (rFilename.test(filename)) {
-        fs.readFile(filepath, 'utf8', function(error, data) {
+        fs.readFile(filename, 'utf8', function(error, data) {
           if (error) {
             console.log(filename + ' not found');
             return callback(error);
@@ -32,7 +30,7 @@
           var content = data.body;
           // search excerpt tag
           if (content && !rExcerpt.test(content)) {
-            invalidPosts.push(filepath);
+            invalidPosts.push(filename);
           }
           return callback();
         })
@@ -40,17 +38,26 @@
     }
 
     var invalidPosts = [];
+    var postsDir = path.resolve(process.cwd(), path.resolve(hexo.config.source_dir, '_tests'));
     // read in posts folder
     fs.readdir(postsDir, function(error, filenames) {
       if (error) {
         return done(error);
       }
+      // resolve path for all filenames
+      filenames = filenames.map(function(filename) {
+        return path.resolve(postsDir, filename);
+      });
       // check each files
       async.forEach(filenames, checkFile, function(error) {
         if (error) {
           throw error;
         }
         else {
+          filenames.forEach(function(filename) {
+            console.log(path.basename(filename));
+          });
+          console.log(filenames.length + ' post(s) found');
           cb(invalidPosts);
         }
       })
@@ -70,31 +77,29 @@
      * @param callback
      */
     function processFile(filename, callback) {
-      if (rFilename.test(filename)) {
-        fs.readFile(filename, 'utf8', function(error, data) {
+      fs.readFile(filename, 'utf8', function(error, data) {
+        if (error) {
+          return callback(filename + ' : can\'t access file');
+        }
+        // regex to search a sentence (japanese, chinese allowed)
+        var rSentence = /([.?!。])\s*(?=[A-Z一-龠ぁ-ゔァ-ヴー々〆〤])/;
+        // get body of post
+        var content = fm(data).body;
+        // get index of the first sentence
+        var index = content.search(rSentence);
+        // insert `<!-- more -->` tag at `index`
+        content = content.slice(0, index + 1) + '<!-- more -->' + content.slice(index + 1);
+        // erase the old body of post with the new
+        data = data.slice(0, data.indexOf(content.slice(0, index))) + content;
+        // write the file with the new data
+        fs.writeFile(filename, data, function(error) {
           if (error) {
-            return callback(filename + ' : can\'t access file');
+            return callback(filename + ' not found');
           }
-          // regex to search a sentence (japanese, chinese allowed)
-          var rSentence = /([.?!。])\s*(?=[A-Z一-龠ぁ-ゔァ-ヴー々〆〤])/;
-          // get body of post
-          var content = fm(data).body;
-          // get index of the first sentence
-          var index = content.search(rSentence);
-          // insert `<!-- more -->` tag at `index`
-          content = content.slice(0, index + 1) + '<!-- more -->' + content.slice(index + 1);
-          // erase the old body of post with the new
-          data = data.slice(0, data.indexOf(content.slice(0, index))) + content;
-          // write the file with the new data
-          fs.writeFile(filename, data, function(error) {
-            if (error) {
-              return callback(filename + ' not found');
-            }
-            console.log(path.basename(filename) + ' : processed');
-            return callback();
-          });
-        })
-      }
+          console.log(path.basename(filename) + ' : processed');
+          return callback();
+        });
+      })
     }
 
     // process each files
@@ -112,12 +117,19 @@
    * Add `<!-- more -->` tag in each post file which doesn't have an excerpt tag
    */
   function autoExcerptMigration(cb) {
+    console.log('------------');
     console.log('Checking for posts without `<-- more -->` and `<!-- excerpt -->` tag...');
+    console.log('------------');
     getInvalidPosts(function(posts) {
-      console.log(posts.length + ' post(s) found');
       if (posts.length) {
+        console.log('------------');
         console.log('Since auto excerpt feature doesn\'t exist anymore in Tranquilpeak 1.7.0');
-        console.log('The \'<!-- more -->\' tag will be inserted at the end of the first sentence of each posts which don\'t have excerpt tag');
+        console.log(
+          'The \'<!-- more -->\' tag will be inserted at the end of the first ' +
+          'sentence of each posts which don\'t have excerpt tag');
+        console.log('------------');
+        console.log('Processing posts...');
+        console.log('------------');
         fixInvalidPosts(posts, function() {
           cb();
         });
