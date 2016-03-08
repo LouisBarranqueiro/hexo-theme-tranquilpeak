01acc11af9eb1783bf914445cacc5ec31e5073b5
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-03-08 21:56:49 +0100

    add prompt to ask date and process all previous post

--- scripts/migrators/1.7.0.js
+++ scripts/migrators/1.7.0.js
@@ -1,16 +1,18 @@
 (function() {
   'use strict';
-
+  
   var fm = require('front-matter');
   var fs = require('fs');
   var path = require('path');
   var async = require('async');
-
+  var prompt = require('prompt');
+  var moment = require('moment');
+  
   /**
    * Return an array with all files which don't have an excerpt tag
    * @param {function} cb a callback
    */
-  function getInvalidPosts(cb) {
+  function searchInvalidPosts(date, cb) {
     /**
      * Check that the file have an excerpt tag
      * @param {String} filename
@@ -21,24 +23,31 @@
       var rFilename = /.md$/;
       // check that file is a markdown file
       if (rFilename.test(filename)) {
-        fs.readFile(filename, 'utf8', function(error, data) {
+        fs.readFile(filename, 'utf8', function(error, post) {
           if (error) {
             console.log(filename + ' not found');
             return callback(error);
           }
-          data = fm(data);
-          var content = data.body;
+          // parse post (front-matter and body)
+          post = fm(post);
+          // parse post date
+          var postDate = moment(post.attributes.date).format('YYYY-MM-DD');
           // search excerpt tag
-          if (content && !rExcerpt.test(content)) {
+          if (post.body && !rExcerpt.test(post.body) &&
+            date.isSameOrAfter(postDate)) {
             invalidPosts.push(filename);
           }
           return callback();
-        })
+        });
       }
     }
-
+    
     var invalidPosts = [];
     var postsDir = path.resolve(process.cwd(), path.resolve(hexo.config.source_dir, '_tests'));
+
+    // parse date (user input)
+    date = moment(date);
+
     // read in posts folder
     fs.readdir(postsDir, function(error, filenames) {
       if (error) {
@@ -53,24 +62,23 @@
         if (error) {
           throw error;
         }
-        else {
-          filenames.forEach(function(filename) {
-            console.log(path.basename(filename));
-          });
-          console.log(filenames.length + ' post(s) found');
-          cb(invalidPosts);
-        }
+        // display all invalid posts name
+        invalidPosts.forEach(function(post) {
+          console.log(path.basename(post));
+        });
+        console.log(invalidPosts.length + ' post(s) found');
+        cb(invalidPosts);
       })
     });
   }
-
+  
   /**
    * Add `<!-- more -->` tag in each post file which doesn't have an excerpt tag
    * @param {Array} posts an array of filepath
    * @param {function} cb a callback
    */
   function fixInvalidPosts(posts, cb) {
-
+    
     /**
      * Add `<!-- more -->` tag in the file
      * @param filename
@@ -101,54 +109,67 @@
         });
       })
     }
-
+    
     // process each files
     async.forEach(posts, processFile, function(error) {
       if (error) {
         throw error;
       }
-      else {
-        cb();
-      }
+      cb();
     })
   }
-
+  
   /**
    * Add `<!-- more -->` tag in each post file which doesn't have an excerpt tag
    */
   function autoExcerptMigration(cb) {
+    var date = {
+      description: 'Enter a date',
+      type: 'string',
+      pattern: /^\d{4}-\d{2}-\d{2}$/,
+      message: 'date format (YYYY-MM-DD)',
+      hidden: false,
+      required: true
+    };
+    
     console.log('------------');
-    console.log('Checking for posts without `<-- more -->` and `<!-- excerpt -->` tag...');
+    console.log('Auto excerpt feature doesn\'t exist anymore since Tranquilpeak 1.4.0');
+    console.log(
+      'To overcome this, the \'<!-- more -->\' tag will be inserted at the end of the first ' +
+      'sentence of each posts which don\'t have excerpt tag');
+    console.log('Provide the date of your last post written with Tranquilpeak < v1.4.0');
     console.log('------------');
-    getInvalidPosts(function(posts) {
-      if (posts.length) {
-        console.log('------------');
-        console.log('Since auto excerpt feature doesn\'t exist anymore in Tranquilpeak 1.7.0');
-        console.log(
-          'The \'<!-- more -->\' tag will be inserted at the end of the first ' +
-          'sentence of each posts which don\'t have excerpt tag');
-        console.log('------------');
-        console.log('Processing posts...');
-        console.log('------------');
-        fixInvalidPosts(posts, function() {
+    // ask a date
+    prompt.get(date, function(error, data) {
+      console.log('Checking for posts without `<-- more -->` and `<!-- excerpt -->` tag...');
+      console.log('------------');
+      searchInvalidPosts(data.question, function(posts) {
+        if (posts.length) {
+          console.log('------------');
+          console.log('Processing posts...');
+          console.log('------------');
+          fixInvalidPosts(posts, function() {
+            cb();
+          });
+        }
+        else {
           cb();
-        });
-      }
-      else {
-        cb();
-      }
+        }
+      });
     });
-
+    
   }
-
+  
   /**
    * Register migrator
    * @param {Array} args
    */
   hexo.extend.migrator.register('1.7.0', function(args) {
     console.log('-> Migration started');
+    prompt.start();
     autoExcerptMigration(function() {
-      console.log('-> Migration finished successfully')
+      console.log('------------');
+      console.log('-> Migration finished')
     });
   });
 })();
