bdd6b44d660b0520f225416bfa60ce26f8053ac2
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-04-17 15:23:02 +0200

    fix spelling mistake in uglify grunt task

--- tasks/config/uglify.js
+++ tasks/config/uglify.js
@@ -3,7 +3,7 @@ var randToken = require('rand-token');
 module.exports = function(grunt) {
   var website = {};
   var token = randToken.generate(60).toLocaleLowerCase();
-  website['source/assets/js/scrip-' + token + '.min.js'] = ['source/assets/js/script.js'];
+  website['source/assets/js/script-' + token + '.min.js'] = ['source/assets/js/script.js'];
   grunt.config.set('uglify', {
     // Minify `script.js` file into `script.min.js`
     prod: {
