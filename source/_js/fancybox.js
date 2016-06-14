6b08bd63ece138eeeda46a03583096d6b6206cbc
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2016-06-14 23:07:24 +0200

    #251 - improve responsiveness of image gallery on small screens

--- source/_js/fancybox.js
+++ source/_js/fancybox.js
@@ -4,30 +4,53 @@
   // Run fancybox feature
 
   $(document).ready(function() {
-    $(".fancybox").fancybox({
-      maxWidth: 900,
-      maxHeight: 800,
-      fitToView: true,
-      width: '50%',
-      height: '50%',
-      autoSize: true,
-      closeClick: false,
-      openEffect: 'elastic',
-      closeEffect: 'elastic',
-      prevEffect: 'none',
-      nextEffect: 'none',
-      padding: '0',
-      helpers: {
-        thumbs: {
+    function fancyFox() {
+      var arrows = true;
+      var thumbs = null;
+
+      // disable navigation arrows and display thumbs on medium and large screens
+      if ($(window).height() > 480) {
+        arrows = false;
+        thumbs = {
           width: 70,
           height: 70
-        },
-        overlay: {
-          css: {
-            background: 'rgba(0, 0, 0, 0.85)'
-          }
-        }
+        };
       }
+
+      $('.fancybox').fancybox({
+        maxWidth: 900,
+        maxHeight: 800,
+        fitToView: true,
+        width: '50%',
+        height: '50%',
+        autoSize: true,
+        arrows: arrows,
+        closeClick: false,
+        openEffect: 'elastic',
+        closeEffect: 'elastic',
+        prevEffect: 'none',
+        nextEffect: 'none',
+        padding: '0',
+        helpers: {
+          thumbs: thumbs,
+          overlay: {
+            css: {
+              overflow: 'hidden',
+              background: 'rgba(0, 0, 0, 0.85)'
+            }
+          }
+        },
+        afterLoad: function() {
+          setTimeout(function() {
+            $('.fancybox-next > span, .fancybox-prev > span').css('visibility', 'visible');
+          }, 400);
+        }
+      });
+    }
+
+    fancyFox();
+    $(window).smartresize(function() {
+      fancyFox();
     });
   });
 })(jQuery);
