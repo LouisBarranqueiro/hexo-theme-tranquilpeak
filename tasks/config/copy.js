0a32fa112fe0a2d6c1af37c4ae1b4b2e4a7f4df0
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2019-05-25 19:09:49 -0700

    chore(deps): update eslint and eslint-config-google deps

--- tasks/config/copy.js
+++ tasks/config/copy.js
@@ -7,58 +7,58 @@ var fontAwesomePath = './node_modules/@fortawesome/fontawesome-free';
 var jqueryPath = './node_modules/jquery';
 
 module.exports = function(grunt) {
-    grunt.config.set('copy', {
-        // Copy all needed files by types
-        dev: {
-            files: [
-                // fancybox
-                {
-                    src: fancyboxPath + '/dist/jquery.fancybox.css',
-                    dest: cssDestFolder,
-                    flatten: true,
-                    expand: true
-                },
-                {
-                    src: fancyboxPath + '/dist/jquery.fancybox.js',
-                    dest: jsDestFolder,
-                    flatten: true,
-                    expand: true
-                },
-                {
-                    src: fancyboxPath + '/src/js/thumbs.js',
-                    dest: jsDestFolder,
-                    flatten: true,
-                    expand: true
-                },
-                {
-                    src: fancyboxPath + '/src/css/thumbs.css',
-                    dest: cssDestFolder,
-                    flatten: true,
-                    expand: true
-                },
-                // font-awesome
-                {
-                  src: fontAwesomePath + '/css/all.css',
-                  dest: cssDestFolder,
-                  flatten: true,
-                  expand: true
-                },
-                {
-                  src: fontAwesomePath + '/webfonts/*',
-                  dest: fontsDestFolder,
-                  flatten: true,
-                  expand: true
-                },
-                // jquery
-                {
-                    src: jqueryPath + '/dist/jquery.js',
-                    dest: jsDestFolder,
-                    flatten: true,
-                    expand: true
-                }
-            ]
+  grunt.config.set('copy', {
+    // Copy all needed files by types
+    dev: {
+      files: [
+        // fancybox
+        {
+          src: fancyboxPath + '/dist/jquery.fancybox.css',
+          dest: cssDestFolder,
+          flatten: true,
+          expand: true
+        },
+        {
+          src: fancyboxPath + '/dist/jquery.fancybox.js',
+          dest: jsDestFolder,
+          flatten: true,
+          expand: true
+        },
+        {
+          src: fancyboxPath + '/src/js/thumbs.js',
+          dest: jsDestFolder,
+          flatten: true,
+          expand: true
+        },
+        {
+          src: fancyboxPath + '/src/css/thumbs.css',
+          dest: cssDestFolder,
+          flatten: true,
+          expand: true
+        },
+        // font-awesome
+        {
+          src: fontAwesomePath + '/css/all.css',
+          dest: cssDestFolder,
+          flatten: true,
+          expand: true
+        },
+        {
+          src: fontAwesomePath + '/webfonts/*',
+          dest: fontsDestFolder,
+          flatten: true,
+          expand: true
+        },
+        // jquery
+        {
+          src: jqueryPath + '/dist/jquery.js',
+          dest: jsDestFolder,
+          flatten: true,
+          expand: true
         }
-    });
+      ]
+    }
+  });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
 };
