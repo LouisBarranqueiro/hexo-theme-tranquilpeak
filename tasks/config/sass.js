be31455f77164041cb624690ed1f66412ae8d74e
Louis Barranqueiro <louis.barranqueiro@gmail.com>
2019-05-25 19:44:26 -0700

    chore(deps): update all dev dependencies

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -1,8 +1,11 @@
+const sass = require('node-sass');
+
 module.exports = function(grunt) {
   grunt.config.set('sass', {
     // Compile `tranquilpeak.scss` file into `tranquilpeak.css`
     dev: {
       options: {
+        implementation: sass,
         sourceMap: false
       },
       files: {
